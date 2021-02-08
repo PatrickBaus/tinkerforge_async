1c87d662e4792e9fde30a009da7fa0a9c5107588
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2021-02-08 23:26:15 +0100

    Code cleanup

--- source/bricklet_motion_detector_v2.py
+++ source/bricklet_motion_detector_v2.py
@@ -129,13 +129,3 @@ class BrickletMotionDetectorV2(BrickletWithMCU):
 
         return GetIndicator(*unpack_payload(payload, 'B B B'))
 
-    def _process_callback(self, header, payload):
-        try:
-            header['function_id'] = self.CallbackID(header['function_id'])
-        except ValueError:
-            # ValueError: raised if the callbackID is unknown
-            raise UnknownFunctionError from None
-        else:
-            payload = unpack_payload(payload, self.CALLBACK_FORMATS[header['function_id']])
-            super()._process_callback(header, payload)
-
