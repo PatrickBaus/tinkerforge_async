e03847c4300695dc44c1e9c984858dccdb9fea7d
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2021-09-26 03:47:32 +0200

    Updated PTC bricklet Updated PTC v2 bricklet

--- tinkerforge_async/bricklet_ptc.py
+++ tinkerforge_async/bricklet_ptc.py
@@ -545,14 +545,27 @@ class BrickletPtc(Device):
             value /= 10
         return int(value * 32768 / 390)
 
-    async def read_events(self):
+    async def read_events(self, events=None, sids=None):
+        registered_events = set()
+        if events:
+            for event in events:
+                registered_events.add(self.CallbackID(event))
+        if sids is not None:
+            for sid in sids:
+                for callback in self.SID_TO_CALLBACK.get(sid, []):
+                    registered_events.add(callback)
+
+        if not events and not sids:
+            for callback in self.SID_TO_CALLBACK.items():
+                registered_events.add(callback)
+
         async for header, payload in super().read_events():
             try:
                 function_id = CallbackID(header['function_id'])
             except ValueError:
                 # Invalid header. Drop the packet.
                 continue
-            if function_id in self._registered_events:
+            if function_id in registered_events:
                 value = unpack_payload(payload, self.CALLBACK_FORMATS[function_id])
                 if function_id in (CallbackID.TEMPERATURE, CallbackID.TEMPERATURE_REACHED):
                     yield self.build_event(0, function_id, self.__value_to_si_temperature(value))
