9e52f4810246bf27e16053fd4b427c01005f11d5
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2022-06-28 10:02:06 +0200

    Fixed missing .

--- tinkerforge_async/__init__.py
+++ tinkerforge_async/__init__.py
@@ -3,4 +3,4 @@ from ._version import __version__
 from .ip_connection import IPConnectionAsync
 from .device_factory import device_factory
 
-from ip_connection_helper import base58encode, base58decode
+from .ip_connection_helper import base58encode, base58decode
