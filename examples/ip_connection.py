09761f7821d5b02b4794c764885874744b5ac7d7
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2022-07-24 23:48:46 +0200

    Updated examples

--- examples/ip_connection.py
+++ examples/ip_connection.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-An example to demonstrate most of the capabilities of the Tinkerforge
-IP connection. It will connect to the remote host and enumerate it and
-list all sensors.
+An example to demonstrate most of the capabilities of the Tinkerforge IP connection. It will connect to the remote host
+and enumerate it to list all sensors.
 """
 import asyncio
 import warnings
@@ -13,25 +11,25 @@ from tinkerforge_async import IPConnectionAsync
 
 async def main():
     """
-    The main loop, that will spawn all callback handlers and wait until they are
-    done. There are two callback handlers, one waits for the bricklet to connect
-    and run the demo, the other handles messages sent by the bricklet.
+    The main loop, that will spawn all callback handlers and wait until they are done. There are two callback handlers,
+    one waits for the bricklet to connect and run the demo, the other handles messages sent by the bricklet.
     """
     try:
-        async with IPConnectionAsync(host='127.0.0.1', port=4223) as ipcon:
-            print('Enumerating brick and waiting for bricklets to reply')
-            await ipcon.enumerate()
-            async for packet in ipcon.read_enumeration():
-                print(packet)
+        async with IPConnectionAsync(host="127.0.0.1", port=4223) as connection:
+            print("Enumerating brick and waiting for bricklets to reply. Press Ctrl+C to terminate.")
+            await connection.enumerate()
+            async for enumeration_type, device in connection.read_enumeration():  # pylint: disable=unused-variable
+                print(device)
 
     except ConnectionRefusedError:
-        print('Could not connect to server. Connection refused. Is the brick daemon up?')
+        print("Could not connect to server. Connection refused. Is the brick daemon up?")
     except asyncio.CancelledError:
-        print('Stopped the main loop')
+        print("Stopped the main loop")
         raise
 
+
 # Report all mistakes managing asynchronous resources.
-warnings.simplefilter('always', ResourceWarning)
+warnings.simplefilter("always", ResourceWarning)
 
 # Start the main loop and run the async loop forever
 asyncio.run(main(), debug=True)
--- tinkerforge_async/brick_master.py
+++ tinkerforge_async/brick_master.py
@@ -303,6 +303,8 @@ class CallbackID(Enum):
     USB_VOLTAGE_REACHED = 64
 
 
+_CallbackID = CallbackID
+
 @unique
 class FunctionID(_FunctionID):
     """
@@ -3526,7 +3528,9 @@ class BrickMaster(DeviceWithMCU):
         return int(value * 1000)
 
     async def read_events(
-        self, events: tuple[int, ...] | list[int] | None = None, sids: tuple[int, ...] | list[int] | None = None
+        self,
+        events: tuple[int | _CallbackID, ...] | list[int | _CallbackID] | None = None,
+        sids: tuple[int, ...] | list[int] | None = None,
     ) -> AsyncGenerator[Event, None]:
         registered_events = set()
         if events:
